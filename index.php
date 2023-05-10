faf7e72d6a7cdeb0272802a75b5cdb2d77e03da3
Jakub Hawryłkowicz <hawrylkowicz.jakub@gmail.com>
2023-05-10 17:43:54 +0200

    Charbot project: index.php, admin.php, styles.php - files new created

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Chatbot</title>
+</head>
+<body>
+    <main>
+        <section>
+            <div class="content">
+                <div class="title">
+                    <h1>chatbot</h1>
+                </div>
+                <form action="">
+                    <div class="userinput">
+                        <h3>question:</h3>
+                        <textarea type="text" name="user" placeholder="type your question"></textarea>
+                        <h3>answer:</h3>
+                        <div class="answer">
+                            
+                        </div>
+                    </div>
+                    <div class="subbutton">
+                        <input type="submit" name="submit" value="ask chatbot">
+                    </div>
+                </form>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
